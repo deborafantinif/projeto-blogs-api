26cd471a9e5a2e008e8df9cc615cd078baeb025e
deborafantinif <deboraferfan@gmail.com>
2022-07-18 21:52:04 -0300

    feat[req11]: create model postCategory

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,47 @@
+'use strict';
+const { DataTypes } = require('sequelize')
+
+/** @type {import('sequelize').ModelAttributes} */
+const attributes = {
+  postId: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    references: {
+      model: 'PostBlog',
+      key: 'id'
+    },
+  },
+  categoryId: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    references: {
+      model: 'Category',
+      key: 'id'
+    },
+  },
+}
+
+/** @param {import('sequelize').Sequelize} sequelize */
+module.exports = (sequelize) => {
+  const postCategory = sequelize.define('PostCategory', attributes, { timestamps: false })
+
+  postCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      foreignKey: 'categoryId',
+      through: postCategory,
+      otherKey: 'postId',
+    })
+
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'posts',
+      foreignKey: 'postId',
+      through: postCategory,
+      otherKey: 'categoryId',
+    })
+  }
+
+  return postCategory;
+};
